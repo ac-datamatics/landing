bf24d71f25f3e3ed2f3032748de691fde764e1ff
Valeria Pineda <57368055+vpinedagon2000@users.noreply.github.com>
2022-04-07 00:11:47 -0500

    remove unused file

--- photos/index.js
+++
@@ -1,8 +0,0 @@
-function def() {
-    const pic = document.getElementById('recording');
-    if(pic.src == "./photos/r1.png") { 
-        pic.src = "./photos/r2.png"
-    } else { 
-        pic.src = "./photos/r1.png"
-    }
-}
